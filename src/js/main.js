c071cd16bdfa2f50fbf9bcc95802c57c8940fdf4
Devid <130362413+Asperk04@users.noreply.github.com>
2026-01-14 12:10:13 +0400

    script for timer(Data)

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
 window.addEventListener('DOMContentLoaded', () =>{
-
+    //TABSS
     const tabs = document.querySelectorAll('.tabheader__item'),
           tabsContent = document.querySelectorAll('.tabcontent'),
           tabsParent = document.querySelector('.tabheader__items');
@@ -37,4 +37,56 @@ window.addEventListener('DOMContentLoaded', () =>{
         }
     });
 
+//TIMER
+
+const deadline = '2026-05-20';
+
+function getTimeReamining(endTime) {
+    const t = Date.parse(endTime) - Date.parse(new Date()),
+           days = Math.floor(t / (1000 * 60 * 60 * 24)),
+           hours =  Math.floor((t/ (1000 * 60 * 60) % 24)),
+           minuts = Math.floor(t/ (1000 * 60) % 60),
+           seconds = Math.floor((t/ 1000) % 60);
+
+     return {
+        'total': t,
+        'days': days,
+        'hours': hours,
+        'minuts': minuts,
+        'seconds': seconds
+     };
+};
+
+function getZero(num){
+    if (num >= 0 && num < 10){
+        return `0${num}`;
+    }else {
+        return num;
+    }
+}
+
+function setClock(selector, endTime){
+    const timer = document.querySelector(selector),
+            days = document.querySelector('#days'),
+            hours = document.querySelector('#hours'),
+            minutes = document.querySelector('#minutes'),
+            seconds = document.querySelector('#seconds'),
+            timeIntervar = setInterval(updateClock, 1000);
+            updateClock();
+
+        function updateClock (){
+            const t = getTimeReamining(endTime);
+            days.innerHTML = getZero(t.days);
+            hours.innerHTML = getZero(t.hours);
+            minutes.innerHTML = getZero(t.minuts);
+            seconds.innerHTML = getZero(t.seconds);
+            
+            if(t.total <= 0 ){
+                clearInterval(timeIntervar);
+            }
+        }
+};
+
+setClock('.timer', deadline);
 });
+
